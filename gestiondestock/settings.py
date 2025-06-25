6a36ce2d63a46193031a7c467d6430fc3990b6ae
Samibouchktat <samibouchktat1@gmail.com>
2025-06-25 20:51:33 +0100

    sara

--- gestiondestock/settings.py
+++ gestiondestock/settings.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
+    'jazzmin',   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -27,7 +28,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'accounts',
     'carbone',
-    
+
 ]
 
 MIDDLEWARE = [
