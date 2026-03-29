f1e0d2491e32bebfd91a62337cb64503e98334b8
Samibouchktat <samibouchktat1@gmail.com>
2026-03-29 15:32:11 +0100

    add production security settings

--- gestiondestock/settings.py
+++ gestiondestock/settings.py
@@ -98,3 +98,10 @@ AUTHENTICATION_BACKENDS = [
     'inventory.authentication.EmailOrSecondaryEmailBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
